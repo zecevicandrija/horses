b5095862eca1d6e04437f47e281e5f1572a72d7f
andrija14 <andrija.zecevic098@gmail.com>
2025-09-05 21:25:48 +0200

    backend595

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-console.log('ADMIN_API_KEY =', process.env.ADMIN_API_KEY);
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
